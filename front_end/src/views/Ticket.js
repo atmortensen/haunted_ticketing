a9447e7cb4ab920c7522d40d6bb041902aeb85b0
Alexander Mortensen <alextmortensen@gmail.com>
2017-09-11 12:24:19 -0600

    Was showing dummy data.

--- front_end/src/views/Ticket.js
+++ front_end/src/views/Ticket.js
@@ -45,34 +45,34 @@ class Ticket extends Component {
 	}
 
 	componentDidMount() {
-		if (!this.state.transaction) {
+		if (!this.props.transaction) {
 			this.props.history.push('/')
 		}
 	}
 
   render() {
-		if (!this.state.transaction) { return <p></p> }
+		if (!this.props.transaction) { return <p></p> }
 
     return (
 			<Template>
 				<Info>Thank you for your purchase. Please print or screen shot this ticket to bring with you. There are no refunds for lost or forgotten tickets. You must arrive within your time slot to redeem your tickets. We look forward to haunting you!</Info>
 				
 				<FlexBox>
-					<QRCode src={'/api/qr/' + this.state.transaction.qr_code} alt="QR Code" />
+					<QRCode src={'/api/qr/' + this.props.transaction.qr_code} alt="QR Code" />
 
 					<div>
-						<p><strong>Name:</strong> { this.state.transaction.customer_name }</p>
-						<p><strong>Number of Tickets:</strong> { this.state.transaction.number_of_tickets }</p> 
+						<p><strong>Name:</strong> { this.props.transaction.customer_name }</p>
+						<p><strong>Number of Tickets:</strong> { this.props.transaction.number_of_tickets }</p> 
 						<p>
 							<strong>Date:</strong> 
-							{ ' ' + moment.unix(this.state.transaction.time_slot.start_time).format('dddd, MMM Do') }
+							{ ' ' + moment.unix(this.props.transaction.time_slot.start_time).format('dddd, MMM Do') }
 						</p>
 						<p>
 							<strong>Time Slot:</strong>
-							{ ' ' + moment.unix(this.state.transaction.time_slot.start_time).format('h:mma') + ' - ' +
-							moment.unix(this.state.transaction.time_slot.end_time).format('h:mma') }
+							{ ' ' + moment.unix(this.props.transaction.time_slot.start_time).format('h:mma') + ' - ' +
+							moment.unix(this.props.transaction.time_slot.end_time).format('h:mma') }
 						</p>
-						<p><strong>Amount Paid:</strong> ${(this.state.transaction.amount_paid / 100).toFixed(2)}</p>
+						<p><strong>Amount Paid:</strong> ${(this.props.transaction.amount_paid / 100).toFixed(2)}</p>
 						<p>
 							<strong>Address:</strong> {' '}
 							<a target="_blank" href="https://www.google.com/maps/place/Haunted+Mansions+of+Albion/">
