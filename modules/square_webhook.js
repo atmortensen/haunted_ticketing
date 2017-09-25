3644ad3f59ac674b2c964433ec049a341a379d40
Alexander Mortensen <alextmortensen@gmail.com>
2017-09-24 22:02:57 -0600

    fix

--- modules/square_webhook.js
+++ modules/square_webhook.js
@@ -35,5 +35,5 @@ module.exports = (req, res) => {
 		// 	transaction_timestamp
 		// ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`, values)
 
-	}, e => res.send(e))
+	}, () => res.json({ error: 'Server error' }))
 }
